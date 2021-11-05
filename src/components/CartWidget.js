b4653198f96ebbc4977871e71bf9bd8c4f32049f
leonardoromero <leonardorommero@gmail.com>
2021-11-04 23:00:03 -0300

    Incorporado el CartWidget.js junto con la landing

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+import { BsFillCartCheckFill } from 'react-icons/bs'
+
+export const CartWidget = () => {
+    return (
+        <>
+            <BsFillCartCheckFill className='text-2xl self-center ml-3'/>
+        </>
+    )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Logo from '../img/logo.png'
+import { CartWidget } from './CartWidget'
 
 export const NavBar = () => {
     return (
-        <div className="navbar mb-2 shadow-lg bg-pink-300 text-neutral-content">
+        <div className="navbar shadow-lg bg-pink-300 text-neutral-content">
           <div className="px-2 mx-2 navbar-start">
             <button class="btn btn-square btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">           
@@ -14,18 +15,19 @@ export const NavBar = () => {
           </div> 
           <div className="px-2 mx-2 navbar-end lg:flex">
             <div className="flex items-stretch">
-              <a className="hidden lg:flex btn btn-ghost btn-sm rounded-btn mx-5 text-base" href="/#">
+              <a className="hidden lg:flex btn btn-ghost rounded-btn mx-5 text-base" href="/#">
                       Home
                     </a> 
-              <a className="hidden lg:flex btn btn-ghost btn-sm rounded-btn mx-5 text-base" href="/#">
+              <a className="hidden lg:flex btn btn-ghost rounded-btn mx-5 text-base" href="/#">
                       Galería
                     </a> 
-              <a className="hidden lg:flex btn btn-ghost btn-sm rounded-btn mx-5 text-base" href="/#">
+              <a className="hidden lg:flex btn btn-ghost rounded-btn mx-5 text-base" href="/#">
                       Contacto
                     </a> 
-              <a className="btn bg-red-500 border-white hover:bg-red-700 btn-sm hover:border-white rounded-btn mx-5 text-base" href="/#">
+              <a className="btn bg-red-500 border-white hover:bg-red-700 hover:border-white rounded-btn mx-5 text-base" href="/#">
                       Agendate!
                     </a>
+              <CartWidget/>
             </div>
           </div> 
         </div>
